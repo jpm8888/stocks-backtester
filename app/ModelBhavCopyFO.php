d260363db8989a32ae5429eb1caf6687da227f62
jpm <jpm8888@gmail.com>
2019-11-27 21:49:55 +0530

    added models.

---
+++ app/ModelBhavCopyFO.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ModelBhavCopyFO extends Model
+{
+    public $timestamps = false;
+    protected $table = 'bhavcopy_fo';
+    protected $guarded = [];
+}
