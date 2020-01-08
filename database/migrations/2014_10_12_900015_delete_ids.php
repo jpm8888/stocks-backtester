f5c5cb221fd1a4b57a4c2331e6672239b2f570d8
Jitendra Maindola <jpm8888@gmail.com>
2020-01-08 21:10:40 +0530

    added new migrations.

---
+++ database/migrations/2014_10_12_900015_delete_ids.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class DeleteIds extends Migration{
+    public function up()
+    {
+        $this->delete_primary_key('bhavcopy_cm');
+        $this->delete_primary_key('bhavcopy_cm');
+        $this->delete_primary_key('bhavcopy_delv_position');
+    }
+
+    private function delete_primary_key($table_name){
+        if (Schema::hasColumn($table_name, 'id')){
+            DB::statement("ALTER TABLE $table_name MODIFY id INT NOT NULL;");
+            DB::statement("ALTER TABLE $table_name DROP PRIMARY KEY;");
+            DB::statement("ALTER TABLE $table_name DROP COLUMN id;");
+        }
+    }
+
+    public function down(){
+
+    }
+}
