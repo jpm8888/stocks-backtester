fa65d6b19933ceb062567ba1c2cdd2d3b08135af
jpm <jpm8888@gmail.com>
2020-01-28 11:36:47 +0530

    added oi calculations.

--- setup/bhavcopy_processed_v1.sql
+++ setup/bhavcopy_processed_v1.sql
@@ -1,6 +1,6 @@
-insert into bhavcopy_processed (symbol, series, open, high, low, close, prevclose, volume, date, dlv_qty, pct_dlv_traded) 
-	select msf.symbol, bc.series, bc.open, bc.high, bc.low, bc.close, bc.prevclose, bc.volume, bc.date, bdp.dlv_qty, bdp.pct_dlv_traded from master_stocks_fo as msf 
-	left join bhavcopy_cm as bc on msf.symbol = bc.symbol 
+insert into bhavcopy_processed (symbol, series, open, high, low, close, prevclose, volume, date, dlv_qty, pct_dlv_traded)
+	select msf.symbol, bc.series, bc.open, bc.high, bc.low, bc.close, bc.prevclose, bc.volume, bc.date, bdp.dlv_qty, bdp.pct_dlv_traded from master_stocks_fo as msf
+	left join bhavcopy_cm as bc on msf.symbol = bc.symbol
 	left join bhavcopy_delv_position as bdp on bc.symbol = bdp.symbol and bc.date = bdp.date and bc.series = bdp.series
 	limit 10;
 
@@ -14,3 +14,8 @@ update bhavcopy_processed as bp set bp.cum_pe_oi = (select sum(bf.oi) from bhavc
 update bhavcopy_processed as bp set bp.cum_ce_oi = (select sum(bf.oi) from bhavcopy_fo as bf where bf.symbol = bp.symbol and bf.date = bp.date and bf.instrument = 'OPTSTK' and bf.option_type = 'CE');
 
 update bhavcopy_processed set pcr = ROUND(cum_pe_oi / cum_ce_oi, 2);
+
+update bhavcopy_processed as bp set bp.max_ce_oi_strike = (select bf.strike_price from bhavcopy_fo as bf where bf.symbol = bp.symbol and bf.date = bp.date and bf.instrument = 'OPTSTK' and bf.option_type = 'CE' order by bf.oi desc limit 1);
+
+update bhavcopy_processed as bp set bp.max_pe_oi_strike = (select bf.strike_price from bhavcopy_fo as bf where bf.symbol = bp.symbol and bf.date = bp.date and bf.instrument = 'OPTSTK' and bf.option_type = 'PE' order by bf.oi desc limit 1);
+
