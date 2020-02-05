ea5e2b6fd6929f45d3e8394167a18e2f8c11e98b
jpm <jpm8888@gmail.com>
2020-02-05 12:18:53 +0530

    updated

--- setup/utils/backup_mysql_data.sh
+++ setup/utils/backup_mysql_data.sh
@@ -1,5 +1,22 @@
+echo "$(date) : starting backup..."
 
-#for backing up from image server.
-#it'll only generate sql insert queries;
-./mysqldump -u test_user -p  --skip-triggers --compact --no-create-info isl_shcema | gzip >  /home/ccellrpf/Videos/temp/15012020_1547.sql.gz
+mysqlDumpPath="/usr/local/apps/mysql/bin/mysqldump";
+username='test_user';
+password='jpm19901'
+databaseName="isl_shcema";
 
+make_backup() {
+    table_name="$1";
+    filename="$table_name-$(date +%Y-%m-%d_%H-%M-%S)";
+    echo $(date) : creating backup $filename;
+    $mysqlDumpPath -u $username -p$password  --skip-triggers --compact --no-create-info $databaseName $table_name | gzip > $filename.sql.gz
+    echo $(date) : $table_name "backup done" >> backup_logs.log;
+}
+
+make_backup bhavcopy_cm
+make_backup bhavcopy_delv_position
+make_backup bhavcopy_fo
+make_backup bhavcopy_processed
+make_backup master_stocks_fo
+
+echo "$(date) : all done for now..."
