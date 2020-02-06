c560507474d89637a1f0f152bb0b5f34d5f4c435
jpm <jpm8888@gmail.com>
2020-02-06 12:06:41 +0530

    added new avg volumes.

--- app/Console/Commands/DataProcessing/tests/UpdateMaxOptionOI.php
+++ app/Console/Commands/DataProcessing/tests/UpdateMaxOptionOI.php
@@ -23,10 +23,6 @@ class UpdateMaxOptionOI extends Command
     // max_ce_oi_strike
     // max_pe_oi_strike
 
-
-    //base query
-    //UPDATE bhavcopy_processed bp inner join (select symbol, date, option_type, sum(oi) as total from bhavcopy_fo where option_type = "XX" group by symbol, date, option_type) x on bp.symbol = x.symbol and bp.date = x.date set bp.cum_fut_oi = x.total where bp.id between 1 and 20;
-
     public function __construct(){
         parent::__construct();
     }
@@ -35,7 +31,7 @@ class UpdateMaxOptionOI extends Command
         $this->info('working...');
 
         ModelBhavcopyProcessed::where('v1_processed', 0)
-//            ->where('id', '<', 423781)
+//            ->where('id', '<', 423781) //for debugging.
             ->chunkById(500, function ($chunks){
                 $last_id = 0;
                 foreach ($chunks as $c) {
@@ -56,7 +52,7 @@ class UpdateMaxOptionOI extends Command
                     $query = "select strike_price from bhavcopy_fo partition($partition_name) where symbol = '$c->symbol' and date = '$c->date' and option_type = '$type' order by oi desc limit 1";
                     $output = DB::select(DB::raw($query));
                     $strike_price = (isset($output[0]->strike_price)) ? $output[0]->strike_price : 0;
-                    DB::statement("update bhavcopy_processed set $column_name = $strike_price, v1_processed = 1  where id = $c->id");
+                    DB::statement("update bhavcopy_processed set $column_name = $strike_price where id = $c->id");
 
                     $last_id = $c->id;
                 }
@@ -65,12 +61,6 @@ class UpdateMaxOptionOI extends Command
 
         $this->info(Carbon::now() . ' : all done');
     }
-
-
-    private function update_strike_price($c, $type, $column_name){
-
-    }
-
 }
 
 
