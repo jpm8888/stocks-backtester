2d54e095feb101dca750ff9bf35ca9c7bcda9946
jpm <jpm8888@gmail.com>
2020-02-08 20:46:28 +0530

    updated.

--- config/database.php
+++ config/database.php
@@ -63,7 +63,7 @@ return [
             ]) : [],
             'modes' => [
                 'ONLY_FULL_GROUP_BY',
-                //'STRICT_TRANS_TABLES',
+                'STRICT_TRANS_TABLES',
                 'NO_ZERO_IN_DATE',
                 'NO_ZERO_DATE',
                 'ERROR_FOR_DIVISION_BY_ZERO',
