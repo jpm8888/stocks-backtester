86c0c779649e11b298251c9ca5e47e0522f9a503
jpm <jpm8888@gmail.com>
2020-02-11 20:11:04 +0530

    added new inconsolas font.

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,5 +1,9 @@
 <html>
-<body style='font-family:"monospace"'>
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
+</head>
+
+<body style="font-family: 'Inconsolata', monospace;">
     <p>{!! $msg !!}</p>
 </body>
 </html>
