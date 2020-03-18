e74a718a37865eb29c18ea7cf59509c56131693c
jpm <jpm8888@gmail.com>
2020-03-18 14:58:48 +0530

    updated script.

--- update_trading_view_lib.sh
+++ update_trading_view_lib.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 cd public
+rm -rf charting_library
+rm -rf trading_view_lib
 git clone https://github.com/tradingview/charting_library.git
 mv charting_library trading_view_lib
 cd trading_view_lib
@@ -9,3 +11,5 @@ git pull
 cp -r charting_library ../
 cp -r datafeeds ../
 cp -r charting_library ../../resources/js/
+echo "everything is done.."
+echo "make sure to npm run !!!"
