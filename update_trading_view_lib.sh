a0f037d889b42943815c09695fa2c87d06d89098
Jitendra Maindola <jpm8888@gmail.com>
2020-03-18 08:04:10 +0530

    updated trading view lib to unstable branch

--- update_trading_view_lib.sh
+++ update_trading_view_lib.sh
@@ -5,20 +5,4 @@ git branch
 git pull
 cp -r charting_library ../
 cp -r datafeeds ../
-cp -r charting_library ../resources/js/
-
-
-#git status
-#git stash
-#git pull
-#composer install --no-dev
-#composer dump-autoload
-#php artisan migrate
-#php artisan cache:clear
-#php artisan route:cache
-#php artisan view:clear
-#php artisan config:cache
-#php artisan config:clear
-#npm install
-#npm run production
-#echo "all done for now..."
+cp -r charting_library ../../resources/js/
