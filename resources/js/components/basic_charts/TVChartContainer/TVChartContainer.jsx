c6ff130f1acf43846a0feac262650366ce5471cd
Jitendra Maindola <jpm8888@gmail.com>
2020-03-19 08:36:59 +0530

    added meta info in separate file.

--- resources/js/components/basic_charts/TVChartContainer/TVChartContainer.jsx
+++ resources/js/components/basic_charts/TVChartContainer/TVChartContainer.jsx
@@ -4,6 +4,7 @@ import {widget} from "../../../charting_library/charting_library.min";
 import connect from "react-redux/es/connect/connect";
 import {bindActionCreators} from "redux";
 import {setWidget} from "../actions/indexActions";
+import {configFutCoi} from "./indicators/fut_coi/meta";
 
 
 function getLanguageFromURL() {
@@ -66,56 +67,7 @@ class TVChartContainer extends Component {
 				// let x = new FutureCoi(PineJS);
 				// let config = configFutCoi;
                 return Promise.resolve([
-					{ name: "Equity",
-						metainfo: {
-							"_metainfoVersion": 40,
-							"id": "Equity@tv-basicstudies-1",
-							"scriptIdPart": "",
-							"name": "Equity",
-							"description": "Equity",
-							"shortDescription": "Equity",
-
-							"is_hidden_study": true,
-							"is_price_study": true,
-							"isCustomIndicator": true,
-
-							"plots": [{"id": "plot_0", "type": "line"}],
-							"defaults": {
-								"styles": {
-									"plot_0": {
-										"linestyle": 0,
-										"visible": true,
-
-										// Make the line thinner
-										"linewidth": 1,
-
-										// Plot type is Line
-										"plottype": 2,
-
-										// Show price line
-										"trackPrice": true,
-
-										"transparency": 40,
-
-										// Set the plotted line color to dark red
-										"color": "#880000"
-									}
-								},
-
-								// Precision is set to one digit, e.g. 777.7
-								"precision": 1,
-
-								"inputs": {}
-							},
-							"styles": {
-								"plot_0": {
-									// Output name will be displayed in the Style window
-									"title": "Equity value",
-									"histogramBase": 0,
-								}
-							},
-							"inputs": [],
-						},   constructor: function() {
+					{ name: "Equity", metainfo : configFutCoi,   constructor: function() {
 							this.init = function(context, inputCallback) {
 								this._context = context;
 								this._input = inputCallback;
--- resources/js/components/basic_charts/TVChartContainer/indicators/fut_coi/meta.js
+++ resources/js/components/basic_charts/TVChartContainer/indicators/fut_coi/meta.js
@@ -1,6 +1,4 @@
 export const configFutCoi = {
-  name: "Equity",
-  metainfo: {
     "_metainfoVersion": 40,
     "id": "Equity@tv-basicstudies-1",
     "scriptIdPart": "",
@@ -12,27 +10,26 @@ export const configFutCoi = {
     "isCustomIndicator": true,
     "plots": [{"id": "plot_0", "type": "line"}],
     "defaults": {
-      "styles": {
-        "plot_0": {
-          "linestyle": 0,
-          "visible": true,
-          "linewidth": 1, // Make the line thinner
-          "plottype": 2, // Plot type is Line
-          "trackPrice": true, // Show price line
-          "transparency": 40,
-          "color": "#880000" // Set the plotted line color to dark red
-        }
-      },
-      "precision": 1, // Precision is set to one digit, e.g. 777.7
-      "inputs": {}
+        "styles": {
+            "plot_0": {
+                "linestyle": 0,
+                "visible": true,
+                "linewidth": 1, // Make the line thinner
+                "plottype": 2, // Plot type is Line
+                "trackPrice": true, // Show price line
+                "transparency": 40,
+                "color": "#880000" // Set the plotted line color to dark red
+            }
+        },
+        "precision": 1, // Precision is set to one digit, e.g. 777.7
+        "inputs": {}
     },
 
     "styles": {
-      "plot_0": {
-        "title": "Equity value", // Output name will be displayed in the Style window
-        "histogramBase": 0,
-      }
+        "plot_0": {
+            "title": "Equity value", // Output name will be displayed in the Style window
+            "histogramBase": 0,
+        }
     },
     "inputs": [],
-  }
 };
