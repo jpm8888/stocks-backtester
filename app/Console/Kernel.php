f2543bb1fd70b475e2b4dc5ef654a2470b320b83
jpm <jpm8888@gmail.com>
2020-08-24 08:26:14 +0530

    update timings

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,21 +31,21 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('download:bhavcopy_cm')->weekdays()->at('09:20');
-        $schedule->command('download:bhavcopy_nse_cm')->weekdays()->at('09:25');
-        $schedule->command('make:stocks_cm')->weekdays()->at('09:32');
-        $schedule->command('download:delv_wise_positions')->weekdays()->at('09:35');
-        $schedule->command('download:bhavcopy_fo')->weekdays()->at('09:40');
+        $schedule->command('download:bhavcopy_cm')->weekdays()->at('08:30');
+        $schedule->command('download:bhavcopy_nse_cm')->weekdays()->at('08:31');
+        $schedule->command('make:stocks_cm')->weekdays()->at('08:32');
+        $schedule->command('download:delv_wise_positions')->weekdays()->at('08:33');
+        $schedule->command('download:bhavcopy_fo')->weekdays()->at('08:34');
 
-        $schedule->command('process:bhavcopy_v1')->weekdays()->at('09:50');
+        $schedule->command('process:bhavcopy_v1')->weekdays()->at('08:35');
 
-        $schedule->command('import:vix_indices_data')->weekdays()->at('09:55');
-        $schedule->command('process:bhavcopy_indices_v1')->weekdays()->at('10:00');
+        $schedule->command('import:vix_indices_data')->weekdays()->at('08:36');
+        $schedule->command('process:bhavcopy_indices_v1')->weekdays()->at('08:37');
 
-        $schedule->command('send_mail:logs')->weekdays()->at('10:10');
+        // $schedule->command('send_mail:logs')->weekdays()->at('10:10');
 
-        $schedule->command('delete:temp')->dailyAt('10:20');
-        $schedule->command('delete:temporary_files')->dailyAt('10:20');
+        $schedule->command('delete:temp')->dailyAt('11:00');
+        $schedule->command('delete:temporary_files')->dailyAt('11:00');
     }
 
     protected function commands(){
